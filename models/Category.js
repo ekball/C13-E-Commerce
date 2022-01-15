ce672478a1426d2d0719bd62819428e4709dc9ce
ekball <90882418+ekball@users.noreply.github.com>
2022-01-15 11:20:46 -0500

    added columns to Category

--- models/Category.js
+++ models/Category.js
@@ -6,8 +6,30 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
-  },
+    // id column
+    id: {
+
+      // define as integer
+      type: DataTypes.INTEGER,
+
+      // not null
+      allowNull: false,
+
+      // make primary key
+      primaryKey: true,
+
+      // turn on auto increment
+      autoIncrement: true 
+    },
+    category_name: {
+
+      // define as integer
+      type: DataTypes.STRING,
+
+      // not null
+      allowNull: false,
+    }
+  }, 
   {
     sequelize,
     timestamps: false,
